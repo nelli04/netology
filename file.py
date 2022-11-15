c8de039df0c332200f36db6a8e45d03babf879f9
nelli04 <nelli_ivanova04@mail.ru>
2022-11-15 16:17:02 +0300

    I

---
+++ file.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+from IPython.display import display
